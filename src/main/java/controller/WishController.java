f76e76dd664682c1fe0597f5f4bacb2fc293a183
shashidhar70 <shashidhargubbala70@gmail.com>
2021-02-28 16:28:17 +0530

    commiting from eclipse to second

--- src/main/java/controller/WishController.java
+++ src/main/java/controller/WishController.java
@@ -19,7 +19,7 @@ public class WishController {
 	}
 	@RequestMapping(method=RequestMethod.GET, value="/welcome")
 	public String load() {
-		System.out.println("welcome page loaded 6");
+		System.out.println("welcome page loaded 7");
 		return "welcome";
 	}
 }
